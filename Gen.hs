75e56e6a4cadcd7fba5daec399f11b6e84e7404a
Ian-Woo Kim <ianwookim@gmail.com>
2020-01-31 16:33:52 -0800

    start project.

---
+++ Gen.hs
@@ -0,0 +1,122 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import qualified Data.HashMap.Strict as HM
+import System.Directory (getCurrentDirectory)
+import System.Environment (getArgs)
+import System.FilePath ((</>))
+--
+import FFICXX.Generate.Builder        ( simpleBuilder )
+import FFICXX.Generate.Code.Primitive ( bool_
+                                      , charpp
+                                      , cppclass, cppclass_
+                                      , cstring, cstring_
+                                      , double, double_
+                                      , int, int_
+                                      , uint, uint_
+                                      , void_, voidp
+                                      )
+import FFICXX.Generate.Config         ( FFICXXConfig(..)
+                                      , SimpleBuilderConfig(..)
+                                      )
+import FFICXX.Generate.Type.Cabal     ( BuildType(..), Cabal(..), CabalName(..) )
+import FFICXX.Generate.Type.Config    ( ModuleUnit(..), ModuleUnitMap(..), ModuleUnitImports(..)
+                                      , modImports
+                                      )
+import FFICXX.Generate.Type.Class     ( Arg(..)
+                                      , Class(..)
+                                      , CTypes(CTDouble)
+                                      , Form(FormSimple)
+                                      , Function(..)
+                                      , ProtectedMethod(..)
+                                      , TopLevelFunction(..)
+                                      , Variable(..)
+                                      )
+import FFICXX.Generate.Type.Config    ( ModuleUnit(..)
+                                      , ModuleUnitImports(..)
+                                      )
+import FFICXX.Runtime.CodeGen.Cxx     ( Namespace(..), HeaderName(..) )
+
+
+
+import qualified Data.HashMap.Strict as HM (fromList)
+import Data.Monoid (mempty)
+--
+import FFICXX.Generate.Builder
+import FFICXX.Generate.Code.Primitive
+import FFICXX.Generate.Type.Cabal     ( AddCInc(..)
+                                      , AddCSrc(..)
+                                      , BuildType(Simple)
+                                      , Cabal(..)
+                                      , CabalName(..)
+                                      )
+import FFICXX.Generate.Type.Config (ModuleUnit(..),ModuleUnitMap(..)
+                                   ,ModuleUnitImports(..))
+import FFICXX.Generate.Type.Class
+import FFICXX.Generate.Type.Module
+import FFICXX.Generate.Type.PackageInterface
+
+
+-- -------------------------------------------------------------------
+-- tmpl-dep-test
+-- -------------------------------------------------------------------
+
+cabal :: Cabal
+cabal =
+  Cabal { cabal_pkgname            = CabalName "HDataFrame"
+        , cabal_version            = "0.0"
+        , cabal_cheaderprefix      = "HDataFrame"
+        , cabal_moduleprefix       = "HDataFrame"
+        , cabal_additional_c_incs  = []
+        , cabal_additional_c_srcs  = []
+        , cabal_additional_pkgdeps = [ CabalName "stdcxx" ]
+        , cabal_license            = Just "BSD3"
+        , cabal_licensefile        = Just "LICENSE"
+        , cabal_extraincludedirs   = []
+        , cabal_extralibdirs       = []
+        , cabal_extrafiles         = []
+        , cabal_pkg_config_depends = []
+        , cabal_buildType          = Simple
+        }
+
+extraDep :: [(String,[String])]
+extraDep = []
+
+extraLib :: [String]
+extraLib = []
+
+
+classes :: [Class]
+classes = []
+
+toplevelfunctions :: [TopLevelFunction]
+toplevelfunctions = []
+
+templates :: [TemplateClassImportHeader]
+templates = []
+
+headers :: [(ModuleUnit, ModuleUnitImports)]
+headers =
+  []
+
+main :: IO ()
+main = do
+  let fficfg = FFICXXConfig {
+                 fficxxconfig_workingDir     = cwd </> "tmp" </> "working"
+               , fficxxconfig_installBaseDir = cwd </> "tmpl-dep-test"
+               , fficxxconfig_staticFileDir  = tmpldir
+               }
+      sbcfg  = SimpleBuilderConfig {
+                 sbcTopModule  = "TmplDepTest"
+               , sbcModUnitMap = ModuleUnitMap (HM.fromList headers)
+               , sbcCabal      = cabal
+               , sbcClasses    = classes
+               , sbcTopLevels  = toplevelfunctions
+               , sbcTemplates  = templates
+               , sbcExtraLibs  = extraLib
+               , sbcExtraDeps  = extraDep
+               , sbcStaticFiles = []
+               }
+
+  simpleBuilder fficfg sbcfg
---
+++ shell.nix
@@ -0,0 +1,54 @@
+{ pkgs ? import <nixpkgs> {}
+, fficxxSrc ? (import ./nix/pinned.nix { inherit pkgs; }).fficxxSrc
+}:
+
+with pkgs;
+
+let
+  # TODO: should be packaged into the upstream nixpkgs.
+  DataFrame = callPackage ./DataFrame/default.nix {};
+
+  newHaskellPackages0 = haskellPackages.override {
+    overrides = self: super: {
+      "fficxx-runtime" = self.callCabal2nix "fficxx-runtime" (fficxxSrc + "/fficxx-runtime") {};
+      "fficxx"         = self.callCabal2nix "fficxx"         (fficxxSrc + "/fficxx")         {};
+    };
+  };
+
+  stdcxxNix = import (fficxxSrc + "/stdcxx-gen/default.nix") {
+    inherit stdenv;
+    haskellPackages = newHaskellPackages0;
+  };
+
+  newHaskellPackages = haskellPackages.override {
+    overrides = self: super: {
+      "fficxx-runtime" = self.callCabal2nix "fficxx-runtime" (fficxxSrc + "/fficxx-runtime") {};
+      "fficxx"         = self.callCabal2nix "fficxx"         (fficxxSrc + "/fficxx")         {};
+      "stdcxx"         = self.callPackage stdcxxNix {};
+    };
+
+  };
+
+  hsenv = newHaskellPackages.ghcWithPackages (p: with p; [
+    cabal2nix
+    cabal-install
+    #
+    fficxx
+    fficxx-runtime
+    formatting
+    monad-loops
+    stdcxx
+  ]);
+
+in
+
+stdenv.mkDerivation {
+  name = "HDataFrame-dev";
+
+  buildInputs = [
+    DataFrame
+    pkgconfig
+    hsenv
+  ];
+
+}
